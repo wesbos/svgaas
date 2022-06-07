1ef31f23bd0db8cfb2e257245a2dbd64e8dd7d48
Wes Bos <wesbos@gmail.com>
2022-06-07 16:34:38 -0400

    neat

---
+++ readme.md
@@ -0,0 +1,10 @@
+## SVGAAS
+
+SVG as a service?
+
+Just a proof of concept
+
+https://svg.wesbos.workers.dev/fire.svg
+
+
+see [./src/index.ts](./src/index.ts) for the juice.
---
+++ src/index.ts
@@ -0,0 +1,41 @@
+export interface Env { }
+
+export interface Icon {
+  id: string;
+  slug: string;
+  title: string;
+  url: string;
+}
+
+export interface SVGAPIREsponse {
+  term: string;
+  count: number;
+  limit: number;
+  start: number;
+  response_time: number;
+  icons: Icon[];
+  next: string;
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const url = new URL(request.url);
+    const path = url.pathname.replace('/', '');
+    if(!path.endsWith('.svg')) {
+      return new Response('You should end your url with .svg', { status: 404 });
+    }
+    const searchTerm = path.replace('.svg', '');
+    // otherwise fetch the SVG
+    // This endpoint will probably change, you are supposed to pay for it
+    const res = await fetch(`https://api.svgapi.com/v1/Ty5WcDa63E/list/?search=${searchTerm}&limit=10`);
+    const data = await res.json() as SVGAPIREsponse;
+    const { icons } = data;
+    console.log(icons)
+    // return a random one
+    return fetch(icons[Math.floor(Math.random() * icons.length)].url);
+  },
+};
