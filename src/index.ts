7a86d50096ae2587600c354213a98fb1ebf9bdc7
Wes Bos <wesbos@gmail.com>
2022-06-08 13:14:03 -0400

    edge cases

--- src/index.ts
+++ src/index.ts
@@ -24,17 +24,38 @@ export default {
     ctx: ExecutionContext
   ): Promise<Response> {
     const url = new URL(request.url);
-    const path = url.pathname.replace('/', '');
+    const path = decodeURIComponent(url.pathname).replace('/', '');
     if(!path.endsWith('.svg')) {
-      return new Response('You should end your url with .svg', { status: 404 });
+      return new Response(/*html*/`
+      <head>
+        <meta charset="UTF-8">
+        <link rel="icon" href="https://fav.farm/💩" />
+      </head>
+      <p>You should end your url with .svg, like <a href="/cat.svg">/cat.svg</a></p>
+      <style>
+        body {
+          text-align: center;
+          font-family: sans-serif;
+          height: 100vh;
+          display: grid;
+          place-items: center;
+          font-size: 2em;
+        }
+      </style>
+
+        `, { status: 404, headers: { 'Content-Type': 'text/html' } });
     }
-    const searchTerm = path.replace('.svg', '');
+    console.log(path);
+    console.log('----')
+    const searchTerm = path.replace('.svg', '').replaceAll(' ', '');
     // otherwise fetch the SVG
     // This endpoint will probably change, you are supposed to pay for it
     const res = await fetch(`https://api.svgapi.com/v1/Ty5WcDa63E/list/?search=${searchTerm}&limit=10`);
     const data = await res.json() as SVGAPIREsponse;
     const { icons } = data;
-    console.log(icons)
+    if(!icons.length) {
+      return fetch(`https://cdn.svgapi.com/vector/94277/blank-file.svg`);
+    }
     // return a random one
     return fetch(icons[Math.floor(Math.random() * icons.length)].url);
   },
